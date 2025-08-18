dc8f6078ccfd47eb865d89cb3de94428a68654d6
Daniel Jeffery <danielajeffery@gmail.com>
2025-08-17 21:54:07 -0700

    Working double jump

--- src/sprites.ts
+++ src/sprites.ts
@@ -1,6 +1,6 @@
 import {
-    EngineObject, gamepadStick, gamepadWasPressed, isUsingGamepad, keyDirection,
-    keyWasPressed,
+    EngineObject, gamepadStick, gamepadWasPressed, gamepadWasReleased, isUsingGamepad, keyDirection,
+    keyWasPressed, keyWasReleased,
     textureInfos,
     TileInfo,
     vec2,
@@ -18,24 +18,22 @@ export class Cat extends EngineObject {
     private speed: number;
     private jumpSpeed: number
     private canDoubleJump: boolean;
-
+    private jumpCount: number;
 
     constructor() {
-        super(vec2(0,0));
+        super(vec2(0, 0));
 
         // @ts-expect-error - textureInfos is any
-        this.tileInfo = new TileInfo(vec2(0,0), vec2(16, 16), textureInfos['black_cat.png']);
+        this.tileInfo = new TileInfo(vec2(0, 0), vec2(16, 16), textureInfos['black_cat.png']);
         this.setCollision()
         this.speed = 0.08; // Set a speed for the cat
         this.jumpSpeed = 0.3
-        this.canDoubleJump = true
+        this.canDoubleJump = false
+        this.jumpCount = 0;
     }
+
     public update(): void {
         super.update()
-
-        if (this.groundObject) {
-            this.canDoubleJump = true;
-        }
 
         const moveInput = isUsingGamepad ? gamepadStick(0) : keyDirection()
         if (moveInput.x !== 0) {
@@ -44,17 +42,26 @@ export class Cat extends EngineObject {
             this.velocity = vec2(0, this.velocity.y);
         }
 
-        if (gamepadWasPressed(0) || keyWasPressed('ArrowUp') || keyWasPressed('w')) {
-            if (this.groundObject) {
-                this.velocity = vec2(this.velocity.x, this.jumpSpeed);
-                this.canDoubleJump = true;
-            } else if (this.canDoubleJump && this.velocity.y <= 0) {
-                this.velocity = vec2(this.velocity.x, this.jumpSpeed);
-                this.canDoubleJump = false;
-            }
+        const jumpButtonPressed = gamepadWasPressed(0) || keyWasPressed('ArrowUp') || keyWasPressed('w')
+        const jumpReleased = gamepadWasReleased(0) || keyWasReleased('ArrowUp') || keyWasReleased('w');
+
+        if (this.groundObject) {
+            this.jumpCount = 0
         }
 
+        if (this.canDoubleJump && jumpButtonPressed && this.jumpCount == 2) {
+            this.velocity = vec2(this.velocity.x, this.jumpSpeed);
+            this.canDoubleJump = false;
+            this.jumpCount += 1
+        } else if (jumpReleased && this.jumpCount == 1) {
+            this.canDoubleJump = true
+            this.jumpCount += 1
+        } else if (jumpButtonPressed && this.jumpCount == 0) {
+            this.velocity = vec2(this.velocity.x, this.jumpSpeed);
+            this.jumpCount += 1;
+        }
     }
+
     public render() {
         super.render();
     }
